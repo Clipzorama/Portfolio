9c8fff85d5240ad7dc8df8bdc6cb4f84b620b54b
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-06-04 18:15:47 -0400

    added animations and starter pages for my main content

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+
+
+function Home() {
+    return (
+        <div className="min-h-screen">
+            {/* First we will add theme toggle */}
+
+            {/* Background effects (Metior shower) */}
+
+            {/* Navbar */}
+            {/* Main Content --> Hero Section, About Section, Skills Section, Projects Section, Contact Section */}
+
+            {/* Simple Footer */}
+        </div>
+    );
+}
+
+
+export default Home
