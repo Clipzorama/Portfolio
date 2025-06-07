b5fcc9f80d09da34b9f8470ddea635249fc5cd9d
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-06-07 14:29:35 -0400

    enabling theme for the website

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from "../components/ThemeToggle";
 
 
 function Home() {
     return (
-        <div className="min-h-screen">
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
             {/* First we will add theme toggle */}
+            <ThemeToggle />
 
             {/* Background effects (Metior shower) */}
 
