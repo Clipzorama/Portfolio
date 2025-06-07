4b4516a55cc7ab58375ec13ad91b234caa257acb
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-06-07 19:08:42 -0400

    small change in note in util

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,7 +2,7 @@ import {clsx} from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 
-// Takes any number of arguments (strings, arrays, conditionals) and intelligently joins them into a single class string.
+// Takes any number of arguments (strings, arrays, conditionals) and adequately joins them into a single class string.
 
 export const cn = (...inputs) => {
     return twMerge(clsx(inputs));
