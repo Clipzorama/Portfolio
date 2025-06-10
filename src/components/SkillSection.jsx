bb8316bbd6cdfa82c8988890814fd580fb3fa4bc
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-06-10 17:58:05 -0400

    editing the skills section

--- src/components/SkillSection.jsx
+++ src/components/SkillSection.jsx
@@ -27,6 +27,11 @@ const skills = [
 export const SkillSection = () => {
     return (
         <section id="skills" className="py-24 px-4 relative bg-secondary/30">
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+                    My <span className="text-primary">Skills</span>
+                </h2>
+            </div>
 
         </section>
     );
