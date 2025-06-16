42bd448291356e20c3c02f27ad8df5930b9c8888
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-06-16 16:20:22 -0400

    working on contact section

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,4 +1,4 @@
-import { Mail } from "lucide-react";
+import { Mail, Phone } from "lucide-react";
 
 
 export const ContactSection = () => {
@@ -19,15 +19,36 @@ export const ContactSection = () => {
                     {/* Left side of the grid */}
                     <div className="space-y-8 ">
                         <h3 className="text-2xl font-semibold mb-6">Contact Information</h3>
+
+                        {/* First Section */}
                         <div className="space-y-6 justify-center">
                             <div className="flex items-start space-x-4">
                                 <div className="p-3 rounded-full bg-primary/10">
-                                    <Mail />
+                                    <Mail className="h-6 w-6 text-primary" />
+                                </div>
+                                <div>
+                                    <h4 className="font-medium">Email</h4>
+                                    <a href="mailto:wchrision@gmail.com" className="text-muted-foreground hover:text-primary transition-colors">wchrision@gmail.com</a>
                                 </div>
                             </div>
                         </div>
+
+                        {/* Second Section */}
+                        <div className="space-y-6 justify-center">
+                            <div className="flex items-start space-x-4">
+                                <div className="p-3 rounded-full bg-primary/10">
+                                    <Phone className="h-6 w-6 text-primary" />
+                                </div>
+                                <div>
+                                    <h4 className="font-medium">Phone</h4>
+                                    <a href="tel:+19293746392" className="text-muted-foreground hover:text-primary transition-colors">+1 (929) 374-6392</a>
+                                </div>
+                            </div>
+                        </div>
+
+
                     </div>
-                    
+
                 </div>
             </div>
         </section>
