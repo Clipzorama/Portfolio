cca5c850059694fb065f71364e597020e275e542
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-06-28 00:00:54 -0400

    added the resume for download

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -31,7 +31,7 @@ export const AboutSection = () => {
                                 Let's Connect
                             </a>
 
-                            <a href="#about" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                            <a href="/Copy of CV.pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                                 Download My CV
                             </a>
                         </div>
